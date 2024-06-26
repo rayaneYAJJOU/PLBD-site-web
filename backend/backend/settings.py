c10cf9c0e530fe91f064a795782ebf750b577d5b
rayaneYAJJOU <rayaneyj0303@gmail.com>
2024-06-26 22:08:40 +0100

    back to sqlite

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -96,13 +96,17 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
+    #'default': {
+    #    'ENGINE': 'django.db.backends.mysql',
+    #    'NAME': os.getenv("DB_NAME"),
+    #    'USER': os.getenv("DB_USER"),
+    #    "PASSWORD": os.getenv("DB_PWD"),
+    #    "HOST": os.getenv("DB_HOST"),
+    #    "PORT": os.getenv("DB_PORT")
+    #},
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PWD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT")
+        "ENGINE": 'django.db.backends.sqlite3',
+        "NAME": BASE_DIR / "db.sqlite3"
     }
 }
 
